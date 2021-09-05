1f23cf8fc19ae514eff4c25a7d371ddab23e17d9
Mubbashir Mustafa <mubbashir10@gmail.com>
2021-09-05 07:33:17 +0500

    Added TOKEN Endpoint doc link

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get("/", function (req, res) {
   res.send("code: " + req.query.code);
 
   // to process the code recieved call the TOKEN endpoint
+  // https://docs.aws.amazon.com/cognito/latest/developerguide/token-endpoint.html
 });
 
 app.listen(3000);
